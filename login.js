205db2bfefcc172ccc068367e66716a85e090225
jot4pe <joaopedro13silva@gmail.com>
2023-04-10 20:03:01 -0300

    VERSAO Nova

--- login.js
+++ login.js
@@ -1,1 +1,11 @@
 //Trabalhando no login
+
+
+//Finalizando programa de login
+function login(nome, email){
+    return 'Bem Vindo'
+}
+
+
+//Sistema de login completo
+//REtornamos o nome de usuario quando logado
